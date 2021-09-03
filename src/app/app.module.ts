bf078e7419092f640521db85261a64b676dc7625
ManuDev03 <manuprajan03@gmail.com>
2021-09-03 13:15:17 +0530

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservableComponent } from './observable/observable.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservableComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
